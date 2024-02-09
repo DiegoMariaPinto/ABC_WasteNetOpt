d6436984e83b20b3274ff2b927460444ee90a2c4
Diego Maria Pinto <68275483+DiegoMariaPinto@users.noreply.github.com>
2024-02-09 13:00:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print_hi('PyCharm')
+
+    dati = pd.read_excel('Data/data_example.xlsx')
